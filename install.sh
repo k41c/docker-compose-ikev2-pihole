6a8ab9573f04fd093f2a4a3a4bc5ba41d067bd03
k41c <51283438+k41c@users.noreply.github.com>
2021-04-27 15:24:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 # create a new sub-network (if not exist)
 docker network inspect vpn-net2 &>/dev/null || 
     docker network create --driver=bridge --subnet=172.25.1.0/24 --gateway=172.25.1.1 vpn-net2
+    
+# run docker-compose
+docker-compose up
